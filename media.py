515cf0e5049ddbe341a313026258e504926e219b
wj lee <mymamyma@gmail.com>
2016-08-23 12:57:08 -0400

    added doc

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
 
 class Movie:
+	""" This class provides a way to store movie related information"""
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
 	def __init__(self, title, storyline, poster_image_url, trailer):
