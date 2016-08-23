b042befcf330772b53827a3866c4794fd4d4fe59
wj lee <mymamyma@gmail.com>
2016-08-23 10:31:18 -0400

    entertainment py avatar added

---
+++ entertainment_center.py
@@ -0,0 +1,10 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
+	"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+	"https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+	"http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+	"http://www.youtube.com/watch?v=-9ceBgWV8io")
