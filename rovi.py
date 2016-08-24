2effad2afec4c6a8f2999752cdb458b2f3421e9d
wj lee <mymamyma@gmail.com>
2016-08-24 18:16:20 -0400

    added parsing function and starting to create request synopsis function

--- rovi.py
+++ rovi.py
@@ -25,7 +25,18 @@ def create_request_url(entitytype, query, api_key, sig):
 # This contains more detailed data such as director, actors, and so on.
 def create_request_url_details(cosmoid, api_key, sig):
 	return "http://api.rovicorp.com/data/v1.1/movie/info?cosmoid={}&country=US&language=en&format=json&apikey={}&sig={}".format(cosmoid, api_key, sig)
-	
+
+def make_connection(request_url):
+	# connect to API to retrieve the data
+	connection = urllib.urlopen(request_url)
+	output = connection.read()
+	connection.close()
+	print output
+
+	# Save the output into json format
+	output = json.loads(output)
+	return output
+
 # This is function is used to request url and extract data from it
 def request_data(entitytype, sig, query):
 	# Setting API key
@@ -36,14 +47,7 @@ def request_data(entitytype, sig, query):
 	print "request url:", request_url
 
 	# Searching Batman related movies
-	connection = urllib.urlopen(request_url)
-	output = connection.read()
-	connection.close()
-
-	# Save the output into json format
-	output = json.loads(output)
-
-	return output
+	return make_connection(request_url)
 
 # This is only used for movies to take more detailed information
 # It uses different request format
@@ -56,11 +60,26 @@ def request_movie(sig, cosmoid):
 	print "request url:", request_url
 
 	# Searching Batman related movies
-	connection = urllib.urlopen(request_url)
-	output = connection.read()
-	connection.close()
-	print output
-	# Save the output into json format
-	output = json.loads(output)
+	return make_connection(request_url)
 
-	return output
+def request_synopsis(sig, cosmoid):
+	# Setting API key
+	api_key, secret_key = reading_api_key()
+
+	# Setting request url
+	request_url = create_request_url_synopsis(cosmoid, api_key, sig)
+	print "request url:", request_url
+
+	# Searching Batman related movies
+	return make_connection(request_url)
+
+def parsing_movies(line):
+	parsed = {}
+	parsed["title"] = line["movie"]["title"]
+	if line["movie"]["directors"]:
+		parsed["director"] = line["movie"]["directors"][0]['name']
+	parsed["producedBy"] = line["movie"]["producedBy"]
+	parsed["releaseYear"] = line["movie"]["releaseYear"]
+	parsed["releasedBy"] = line["movie"]["releasedBy"]
+	parsed["rating"] = line["movie"]["rating"]
+	return parsed
