21f6c3b0d50ea05a7237b7b78f3b172a171ec8df
wj lee <mymamyma@gmail.com>
2016-08-24 16:28:47 -0400

    changed API from tmdb to Rovi

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,86 @@
+# -*- coding: utf8 -*-
 import fresh_tomatoes
 import media
-# This library that collects movie data
-from tmdb3 import set_key
+import urllib
+import json
+import pprint
+import os
 
 # This function will bring api key from the local file
 def reading_api_key():
-	with open("C:/tmdb_api_key") as F:
+	lis = []
+	with open("C:/rovi_api_key") as F:
 		for i in F:
-			return i.strip()
+			lis.append(i.strip())
+	return lis[0], lis[1]
 
-# Setting API key
-api_key = reading_api_key()
-print "api_key:",api_key
+# Creating urls to request
+def create_request_url(entitytype, query, api_key, sig):
+	return "http://api.rovicorp.com/search/v2.1/video/search?entitytype={}&query={}&rep=1&size=20&offset=0&language=en&country=US&format=json&apikey={}&sig={}".format(entitytype, query, api_key, sig)
 
-set_key(api_key)
+def create_request_url_details(query, api_key, sig):
+	return "http://api.rovicorp.com/data/v1.1/movie/info?cosmoid={}&country=US&language=en&format=json&apikey={}&sig={}".format(query, api_key, sig)
+	
+# This is function is used to request url and extract data from it
+def request_API(sig, query, id):
+	# Setting API key
+	api_key, secret_key = reading_api_key()
+	print "api_key:",api_key
+	print "secret key:",secret_key
 
-"""
-Chaching Engine: in order to limit excessive 
-usage against the online API server
-"""
-from tmdb3 import set_cache
-set_cache('null')
-set_cache(filename='/full/path/to/cache') # the 'file' engine is assumed
-set_cache(filename='tmdb3.cache') # relative paths are put in /tmp
-set_cache(engine='file', filename='~/.tmdb3cache')
-from tmdb3 import get_locale, set_locale
-set_locale()
+	# Setting query
+	query = "batman"
 
-### Searching Movies
-from tmdb3 import searchMovie
-res = searchMovie('asdf')
-print res
+	# Setting request url
+	request_url = "http://api.rovicorp.com/search/v2.1/video/search?entitytype=movie&query={}&rep=1&size=20&offset=0&language=en&country=US&format=json&apikey={}&sig={}".format(query, api_key, sig)
+	print request_url
+
+	# Searching Batman related movies
+	connection = urllib.urlopen(request_url)
+	output = connection.read()
+	connection.close()
+
+# Rovi is the API for video data. It uses sig variable that is only usuable for 5 minutes
+# once it is created. So instead of calling API everytime I run this script, I will save the result
+# into the local directory and use the saved file when I rerun the script.
+if "movies.json" in os.listdir(r"./"):
+	print("\nmovies.json is in the current directory.\n")
+	with open("movies.json") as F:
+		for i in F:
+			output = json.loads(i)
+			break
+else:
+	# Setting API key
+	api_key, secret_key = reading_api_key()
+	print "api_key:",api_key
+	print "secret key:",secret_key
+	sig = "4ff6e610fefc9aa453a493854ce9e80b"
+	# Setting query
+	query = "batman"
+
+
+
+	# Searching Batman related movies
+	connection = urllib.urlopen(request_url)
+	output = connection.read()
+	connection.close()
+
+	# Save the output into json format
+	output = json.loads(output)
+
+	# We are only interested in title and id from this output
+	results = output['searchResponse']['results']
+	for i in results:
+		print i['video']['masterTitle']
+		print i['id']
+
+	# Save Json file to local drive
+	with open("movies.json", "w") as F:
+		json.dump(output, F)
+
+output = json.loads(output)
+# pprint.pprint(output)
+
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
 	"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
