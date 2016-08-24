228215caed8a05e94b2ef327aeeb12f4e05bc6ec
wj lee <mymamyma@gmail.com>
2016-08-24 16:29:01 -0400

    fixed classes

--- media.py
+++ media.py
@@ -1,21 +1,23 @@
 import webbrowser
 
 class Video():
-	def __init__(self, title, duration):
+	def __init__(self, title, storyline, poster_image_url):
 		self.title = title
-		self.duration = duration
+		self.storyline = storyline
+		self.poster_image_url = poster_image_url
 
 class Movie(Video):
 	""" This class provides a way to store movie related information"""
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
 	def __init__(self, title, storyline, poster_image_url, trailer):
-		self.title = title
-		self.storyline = storyline
-		self.poster_image_url = poster_image_url
+		Video.__init__(self, title, storyline, poster_image_url)
 		self.trailer_youtube_url = trailer
 
 	def show_trailer(self):
-		webbrowser.open(self.trailer)
+		webbrowser.open(self.trailer_youtube_url)
 
 class TvShow(Video):
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+	def __init__(self, title, storyline, poster_image_url, trailer):
+		Video.__init__(self, title, storyline, poster_image_url)
+		self.num_seasons = num_seasons
